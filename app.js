f25a9ac3180367dff8ddd216550ffb33b43507a2
Graillter <timotheehongre@gmail.com>
2020-03-11 15:36:22 +0000

    First

---
+++ app.js
@@ -0,0 +1,22 @@
+var firstName = "Tim";
+var favouriteColor = "Orange";
+var favouriteFood = "Cauliflower";
+var city = "La Rochelle";
+
+// Display the var firstName, favouriteColor and favouriteFood in the terminal
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood);
+
+var phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+var endingPhrase = "is what we see. Rainbow colours, are made with light bending you see!";
+var one = "Red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indigo";
+var seven = "violet";
+
+var sentence = phrase + " " + one + ", " + two + ", " + three + ", " + four + ", " + five + ", " + six + ", " + seven + " " + endingPhrase;
+console.log(sentence);
